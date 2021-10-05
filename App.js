b7d5221d4670238a65247964158ef1cf0e6aa114
roseda1318 <roseda1318@gmail.com>
2021-10-05 16:30:40 -0400

    test commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hopefully this works.!</Text>
+      <Text>This is a test to check commit</Text>
       <StatusBar style="auto" />
     </View>
   );
