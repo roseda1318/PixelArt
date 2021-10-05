792a419c1d6092f889dbb7ec5ad1cd34bcd5c4a0
roseda1318 <roseda1318@gmail.com>
2021-10-05 16:12:50 -0400

    Danielle Merge

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>JASON! Please please please work</Text>
       <StatusBar style="auto" />
     </View>
   );
