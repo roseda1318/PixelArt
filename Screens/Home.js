592c5212ace7b17c349f4414c7bde1dd3a6b0083
roseda1318 <roseda1318@gmail.com>
2021-10-16 16:22:36 -0400

    Start Grid Work

--- Screens/Home.js
+++ Screens/Home.js
@@ -5,11 +5,12 @@ import {
     View,
     Text,
     Button,
-    TouchableOpacity
+    TouchableOpacity, 
+    SafeAreaView
 } from 'react-native';
 
 import { Feather } from '@expo/vector-icons';
-
+var colorOne = 'FF9225';
 
 const home = ({route, navigation}) => {
       navigation.setOptions({
@@ -32,19 +33,114 @@ const home = ({route, navigation}) => {
           </TouchableOpacity>
         ),
          }); 
+        //do an onPress to pass the color code in for each  row/col? Use state variables for colors in containers
     return (
-                       
-            <View>
-                <Text>Home Screen</Text>
-            </View>
-     
-        
-          )
-    } ;   
+
+      <SafeAreaView style={styles.container}>
+        <View style={styles.row}>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R1_}>
+            <Text style={styles.text}>R1.8</Text>
+          </TouchableOpacity>
+      </View>
+
+      <View style={styles.row}>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.R2_}>
+            <Text style={styles.text}>R2.8</Text>
+          </TouchableOpacity>
+      </View>
+   
+    </SafeAreaView>
+  );
+};
+
     
+const button = {
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginRight: 1,
+  borderWidth: 1
+};
+
+const styles = StyleSheet.create({
+container: {
+    flexDirection: "column", // default
+    justifyContent: "flex-start", //default 
+    flex: 1,
+    backgroundColor: colorOne
+
+},
+row: {
+    flexDirection: 'row',
+    flex: 1,
+    marginBottom: 1,
+},
+currentVal: {
+    color: '#fff',
+    flex: 1, 
+    textAlign: 'right',
+    fontSize: 50,
+    marginRight: 10,
+    alignSelf: 'center',
     
-    const styles = StyleSheet.create({});
-    
+},
+
+R1_: {
+  ...button,
+  backgroundColor: "#FF9225",
+},
+R2_: {
+  ...button,
+  backgroundColor: "#FF9225",
+},
+text: {
+  color: "#fff",
+  fontSize: 35,
+},
+});
+
     export default home;
     
   
